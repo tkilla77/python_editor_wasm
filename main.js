e5f791479e6ef8058de57e48e316068e9820b9de
Tom Hofmann <tom@scheidweg.net>
2024-02-10 09:43:48 +0100

    upgrade to pyiodide 2.25.0

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function clearHistory() {
 // init Pyodide and show sys.version when it's loaded successfully
 async function main() {
   let pyodide = await loadPyodide({
-    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.2/full/",
+    indexURL: "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/",
   });
   pyodide.runPython(`
       import sys
