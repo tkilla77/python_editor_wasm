aea8e224c2f7c27bc23a883385731126f239c37f
Tom Hofmann <tom@scheidweg.net>
2024-05-21 17:14:42 +0200

    more publishing voes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/editor.js'),
+      name: 'BottomEditor',
+      // the proper extensions will be added
+      fileName: 'bottom-editor',
+    },
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'embed2.html'),
